1ac58c02a236625750b942ec892256e663c75ed2
JayKuoNoob <jaykuo1203@gmail.com>
2023-08-04 23:58:29 +0800

    edited Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,12 @@ public class Main {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-            // Prompt for directory path
             System.out.print("Please enter the directory path: ");
             String directoryPath = reader.readLine();
 
-            // Prompt for output file name
             System.out.print("Please enter the output file name: ");
             String outputFileName = reader.readLine();
-            String outputFilePath = directoryPath + "\\" + outputFileName + ".txt";  // Add the .txt extension
+            String outputFilePath = directoryPath + "\\" + outputFileName + ".txt";
 
             File directory = new File(directoryPath);
             File[] files = directory.listFiles();
@@ -20,7 +18,6 @@ public class Main {
             FileWriter fileWriter = new FileWriter(outputFilePath);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-            // Write file names to the output file
             for (File file : files) {
                 bufferedWriter.write(file.getName());
                 bufferedWriter.newLine();
